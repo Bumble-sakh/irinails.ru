84d528e4465dbb63e9d1d2ed371ae22fbecd1307
Bumble-sakh <bumble.sakh@gmail.com>
2021-10-25 01:23:05 +1100

    add local files

--- js/script.js
+++ js/script.js
@@ -1,37 +1,40 @@
 /*==================== ACTIVE MENU LINK ====================*/
-const menuItems = document.querySelectorAll('.nav-link');
-const collapseArea = document.querySelector('.collapse');
+const menuItems = document.querySelectorAll(".nav-link");
+const collapseArea = document.querySelector(".collapse");
 
 function menuItemActive(event) {
-    for (const item of menuItems) {
-        item.classList.remove('active');
-    }
-    event.currentTarget.classList.add("active");
-    collapseArea.classList.remove('show')
+  for (const item of menuItems) {
+    item.classList.remove("active");
+  }
+  event.currentTarget.classList.add("active");
+  collapseArea.classList.remove("show");
 }
 
-for (const item of  menuItems) {
-    item.addEventListener('click', menuItemActive);
+for (const item of menuItems) {
+  item.addEventListener("click", menuItemActive);
 }
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('[id]');
+const sections = document.querySelectorAll("[id]");
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 300;
-        sectionId = current.getAttribute('id')
-        console.log(sectionId);
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 300;
+    sectionId = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('a[href*=' + sectionId + ']').classList.add('active')
-        }else{
-            document.querySelector('a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector("a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
 }
 
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
